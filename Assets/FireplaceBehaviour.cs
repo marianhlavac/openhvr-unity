ffc0921d7cd0451c1fc65ff3acabc746b38e6bad
Marián Hlaváč <hlavac95@gmail.com>
2020-05-04 20:44:18 +0200

    feat: Add fireplace behaviour scripts

---
+++ Assets/FireplaceBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireplaceBehaviour : MonoBehaviour
+{
+    public GameObject fireEffect;
+    public void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.name.StartsWith("Match"))
+        {
+            foreach (ParticleSystem effect in fireEffect.GetComponentsInChildren<ParticleSystem>())
+            {
+                effect.Play();
+            }
+        }
+    }
+
+
+}
---
+++ Assets/MatchStickBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchStickBehaviour : MonoBehaviour
+{
+    public GameObject fireEffect;
+    public void Burn()
+    {
+        foreach (ParticleSystem effect in fireEffect.GetComponentsInChildren<ParticleSystem>()) {
+            effect.Play();
+        }
+    }
+    public void PutOut()
+    {
+        foreach (ParticleSystem effect in fireEffect.GetComponentsInChildren<ParticleSystem>())
+        {
+            effect.Stop();
+        }
+    }
+}
